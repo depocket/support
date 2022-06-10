8fc3c07fd17e90b358853ce19ffd3faae694315f
Himitsuko <himitsuko@github.com>
2022-06-11 00:43:55 +0700

    fix: Dynamic type pointer

--- dynamic/dynamic.go
+++ dynamic/dynamic.go
@@ -17,20 +17,21 @@ func (s *Scheme[T]) RegisterService(name string, t T) {
 func (s *Scheme[T]) New(name string) (T, error) {
 	t, ok := s.types[name]
 	if !ok {
-		return nil, fmt.Errorf("unrecognized type name: %s", name)
+		var x T
+		return x, fmt.Errorf("unrecognized type name: %s", name)
 	}
-	return reflect.New(reflect.TypeOf(t)).Interface().(T), nil
+	return reflect.New(reflect.TypeOf(t).Elem()).Interface().(T), nil
 }
 
 func (s *Scheme[T]) TotalServices() int {
-	return len[T](s.types)
+	return len(s.types)
 }
 
 func (s *Scheme[T]) ServicesNames() []string {
 	return lo.Keys[string, T](s.types)
 }
 
-func NewScheme[T]() *Scheme[T] {
+func NewScheme[T any]() *Scheme[T] {
 	return &Scheme[T]{
 		types: map[string]T{},
 	}
