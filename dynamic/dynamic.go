0b1c097565a2c0e456c809f4d49074ad685a40f0
Himitsuko <himitsuko@github.com>
2022-06-10 23:08:28 +0700

    drop: Unused code
    add: Get functions
    
    add: Generic type pointer
    
    fix: Back to generic type only
    
    chore: use interface replace for any

--- dynamic/dynamic.go
+++ dynamic/dynamic.go
@@ -2,6 +2,7 @@ package dynamic
 
 import (
 	"fmt"
+	"github.com/samber/lo"
 	"reflect"
 )
 
@@ -13,12 +14,20 @@ func (s *Scheme[T]) RegisterService(name string, t T) {
 	s.types[name] = t
 }
 
-func (s *Scheme[T]) New(name string) (T, error) {
+func (s *Scheme[T]) New(name string) (*T, error) {
 	t, ok := s.types[name]
 	if !ok {
 		return nil, fmt.Errorf("unrecognized type name: %s", name)
 	}
-	return reflect.New(reflect.TypeOf(t)).Interface().(T), nil
+	return reflect.New(reflect.TypeOf(t)).Interface().(*T), nil
+}
+
+func (s *Scheme[T]) TotalServices() int {
+	return len[T](s.types)
+}
+
+func (s *Scheme[T]) ServicesNames() []string {
+	return lo.Keys[string, T](s.types)
 }
 
 func NewScheme[T]() *Scheme[T] {
