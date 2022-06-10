5324810e7d42c56371caf372e44c5bdcc784bc53
Himitsuko <himitsuko@github.com>
2022-06-10 23:18:48 +0700

    add: Generic type pointer

--- dynamic/dynamic.go
+++ dynamic/dynamic.go
@@ -10,16 +10,16 @@ type Scheme[T any] struct {
 	types map[string]T
 }
 
-func (s *Scheme[T]) RegisterService(name string, t T) {
-	s.types[name] = t
+func (s *Scheme[T]) RegisterService(name string, t *T) {
+	s.types[name] = *t
 }
 
-func (s *Scheme[T]) New(name string) (T, error) {
+func (s *Scheme[T]) New(name string) (*T, error) {
 	t, ok := s.types[name]
 	if !ok {
 		return nil, fmt.Errorf("unrecognized type name: %s", name)
 	}
-	return reflect.New(reflect.TypeOf(t)).Interface().(T), nil
+	return reflect.New(reflect.TypeOf(t)).Interface().(*T), nil
 }
 
 func (s *Scheme[T]) TotalServices() int {
