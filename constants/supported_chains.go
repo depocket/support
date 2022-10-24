6665501f02842dc7f2cd30da1c229404f25df744
Himitsuko <himitsuko@github.com>
2022-10-24 16:59:20 +0700

    add: Sharing supported chains env

---
+++ constants/supported_chains.go
@@ -0,0 +1,40 @@
+package constants
+
+var (
+	SupportedChains = map[string]string{
+		"bsc":       "bsc",
+		"ethereum":  "ethereum",
+		"moonriver": "moonriver",
+		"avalanche": "avalanche",
+		"aurora":    "aurora",
+		"moonbeam":  "moonbeam",
+		"fantom":    "fantom",
+		"arbitrum":  "arbitrum",
+		"polygon":   "polygon",
+		"gnosis":    "gnosis",
+		"optimism":  "optimism",
+		"harmony":   "harmony",
+		"celo":      "celo",
+		"fuse":      "fuse",
+		"coinex":    "coinex",
+		"iotex":     "iotex",
+	}
+	InternalKeysMap = map[string]string{
+		"binance-smart-chain": "bsc",
+		"ethereum":            "ethereum",
+		"moonriver":           "moonriver",
+		"avalanche":           "avalanche",
+		"aurora":              "aurora",
+		"moonbeam":            "moonbeam",
+		"fantom":              "fantom",
+		"arbitrum":            "arbitrum",
+		"polygon":             "polygon",
+		"gnosis":              "gnosis",
+		"optimism":            "optimism",
+		"harmony":             "harmony",
+		"celo":                "celo",
+		"fuse":                "fuse",
+		"coinex":              "coinex",
+		"iotex":               "iotex",
+	}
+)
--- go.mod
+++ go.mod
@@ -4,4 +4,11 @@ go 1.18
 
 require github.com/samber/lo v1.21.0
 
-require golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.10.25 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/exp v0.0.0-20220426173459-3bcf042a4bf5 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
